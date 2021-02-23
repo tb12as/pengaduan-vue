75e396b6c7eb40a47d4a94bcc200dd6d8cc7cf28
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2021-02-23 23:59:44 +0700

    trying to fix cors problem (but didn't work :v)

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,6 +22,8 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 window.axios.defaults.withCredentials = true;
 
 /**
